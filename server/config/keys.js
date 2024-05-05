f1cf557f1ec1030bf246a65e7e49615750f83d56
Rehanuddin Shaik <rehanuddin1024@gmail.com>
2024-05-05 13:11:15 -0600

    W

---
+++ server/config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: "mongodb+srv://shaikruhajabeen:042TaadN577q9jIs@cluster0.s2yzbth.mongodb.net/online-exam-proctoring?retryWrites=true&w=majority",
+    secretOrKey: "secret",
+  };
